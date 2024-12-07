311eb7b584874f5dce7d0d030083705edcad1cf3
M1inhDuk <duccmgch221016@fpt.edu.vn>
2024-12-07 14:45:10 +0700

    Gateway Compelete

--- Gateway/Controllers/WeatherForecastController.cs
+++ Gateway/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace URLshorten.Controllers
+namespace Gateway.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- URLshorten/Migrations/20241207032431_ThirdMigration.Designer.cs
+++ URLshorten/Migrations/20241207032431_ThirdMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using URLshorten.Data;
 
@@ -11,9 +12,11 @@ using URLshorten.Data;
 namespace URLshorten.Migrations
 {
     [DbContext(typeof(URLshortenContext))]
-    partial class URLshortenContextModelSnapshot : ModelSnapshot
+    [Migration("20241207032431_ThirdMigration")]
+    partial class ThirdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
